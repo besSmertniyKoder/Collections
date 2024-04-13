5f024f384353e0961d8b6c5a9b453cbc7c41dd4a
Roman Sukhachev <rsukhachev@bostonsd.ru>
2024-04-13 10:21:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "hello";
+
+        String s2 = new String("hello");
+
+        String s3 = s1;
+
+        String s4 = "h" + "e" + "l" + "l" + "o";
+
+        String s5 = new String("hello");
+
+        String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
+
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+        System.out.println(s1==s4);
+        System.out.println(s1==s5);
+        System.out.println(s1==s6);
+        System.out.println("//////////////");
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+        System.out.println(s1.equals(s5));
+        System.out.println(s1.equals(s6));
+        Object[] objects = {"fjkaj", 490, true};
+
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+
+        arrayList.add("true");
+        arrayList.add("Hello");
+        arrayList.add("hjkhg");
+        arrayList.remove(0);
+        arrayList.remove("true");
+        Collections.sort(arrayList);
+
+
+
+
+    }
+}
